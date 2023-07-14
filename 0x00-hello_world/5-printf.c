9f9e72f6f86999089622ccf85f1b8b74dfb038d7
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-13 23:27:32 -0700

    updated5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-/* A C program that prints output using function printf */
+/**
+ * A C program that prints output using function printf.
+ *
+ * Description: Printf displays the result.
+ */
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
