107eb434938f0a7f8c83567307043b58cc739db7
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-13 22:09:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* A C program that prints output using function printf */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
