0e3591744e25dcdbf322b22748ba55b2676275b3
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-15 18:49:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point.
+ * printf: This prints stringed output
+ *
+ * Description: This prints a string without an escape to a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
