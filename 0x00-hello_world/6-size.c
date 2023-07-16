b2285ff011b84af163eeba595b4b9b3bda7c684d
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-16 01:10:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,8 @@
 /**
  * printf: This prints the output.
  * @a: This is the unsigned int.
- * @i: This is the unsigned long int.
- * @d: This represents the unsigned double.
+ * @j: This is the unsigned float.
  * @c: This represents the unsigned char.
- * @j: This represent the unsigned float.
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -18,8 +15,9 @@ int main(void)
 	int d;
 	char c;
 	float j;
-	printf("The size of an int is: %u.\n", (unsigned)sizeof(a));
-	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(i));
+
+	printf("The size of an int is: %u.\n", (unsigned int)sizeof(a));
+	printf("The size of a long int is: %lu.\n", (long unsigned)sizeof(i));
 	printf("The size of a double is: %lu.\n", (long unsigned)sizeof(d));
 	printf("The size of a char is: %u.\n", (unsigned char)sizeof(c));
 	printf("The size of a float is: %u.\n", (unsigned)sizeof(j));
