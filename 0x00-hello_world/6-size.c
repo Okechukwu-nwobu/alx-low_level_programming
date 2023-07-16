3ad34b18cd443900fcadb131b295f9babdef6149
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-16 02:03:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-/* main - This is the entry point of the program. */
+/* main - This is the entry point */
 /**
+ * sizeof: This evaluates the size of a variable.
  * printf: This prints the output.
- * @a: This is the unsigned int.
- * @j: This is the unsigned float.
- * @c: This represents the unsigned char.
- * Return: Always 0 (Success)
+ *
+ *Description: The program evaluates and prints various sizes.
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
-	int i;
-	int d;
-	char c;
-	float j;
+	int intType;
+	char charType;
+	long int longType;
+	long long int longlongType;
+	float floatType;
 
-	printf("The size of an int is: %u.\n", (unsigned int)sizeof(a));
-	printf("The size of a long int is: %lu.\n", (long unsigned)sizeof(i));
-	printf("The size of a double is: %lu.\n", (long unsigned)sizeof(d));
-	printf("The size of a char is: %u.\n", (unsigned char)sizeof(c));
-	printf("The size of a float is: %u.\n", (unsigned)sizeof(j));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
-	
