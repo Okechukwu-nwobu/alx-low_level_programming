6cfaa3767442916b4d8dd25acafecbacbbee8b02
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-15 18:27:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *puts: Prints a stringed output.
+ *
+ *Description: Prints an output that includes an escape to a new line. '\n'.
+ *Return: Always 0 (Success)
+ */
+int main()
+{
+	puts( "\"Programming is like building a multilingual puzzle");
+	getchar();
+	return (0);
+}
