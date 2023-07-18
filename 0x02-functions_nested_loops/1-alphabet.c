39d1d0df3704af1353b5adee0c77bdd980c5c3ec
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-07-18 11:42:28 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,5 @@
 0x02. C - Functions, nested loops
+A  function that prints the alphabet, in lowercase, followed by a new line.
+
+Prototype: void print_alphabet(void);
+You can only use _putchar twice in your code.
