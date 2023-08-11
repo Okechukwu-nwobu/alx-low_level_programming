3f1ccc9540acfe15ecbd77970558b1ee12225669
Okechukwu Nwobu <nwobuokechukwu@gmail.com>
2023-08-11 02:48:47 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,30 +16,48 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	if (s1 == 0)
+	{
 		return ("");
+	}
 	if (s2 == 0)
+	{
 		return ("");
+	}
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		len1++;
+	}
 	for (i = 0; s2[i] != '\0'; i++)
+	{
 		len2++;
+	}
 	str = malloc(sizeof(char) * (len1 + n) + 1);
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	if (n < len2)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
+		{
 			str[i] = s1[i];
+		}
 		for (i = 0; i < n; i++)
+		{
 			str[len1 + i] = s2[i];
+		}
 		str[len1 + i] = '\0';
 	}
 	else if (n >= len2)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
+		{
 			str[i] = s1[i];
+		}
 		for (i = 0; s2[i] != '\0'; i++)
+		{
 			str[len1 + i] = s2[i];
+		}
 		str[len1 + i] = '\0';
 	}
 	return (str);
